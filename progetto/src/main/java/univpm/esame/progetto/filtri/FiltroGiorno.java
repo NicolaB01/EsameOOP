4c003e53fb6c61a435af41e12a9274288d540ff4
Simone Di Battista <95362468+diba01@users.noreply.github.com>
2022-01-18 11:55:42 +0100

    commit filtri part 6
    FiltroGiorno

---
+++ progetto/src/main/java/univpm/esame/progetto/filtri/FiltroGiorno.java
@@ -0,0 +1,69 @@
+package univpm.esame.progetto.filtri;
+
+import java.util.List;
+import java.util.Vector;
+
+import univpm.esame.progetto.model.Evento;
+
+
+/**
+ * Classe implementata per effettuare un filtro in base ai giorni
+ * 
+ * @author Simone Di Battista
+ *
+ */
+public class FiltroGiorno extends Filtro implements InterfacciaFiltri{
+
+	private String[] giorno;
+
+	/**
+	 * Costruttore di default
+	 */
+	public FiltroGiorno() {
+		super();
+	}
+	
+	/**
+	 * 
+	 * @param giorni I giorni da filtrare scritte in CSV (separati da virgola)
+	 */
+	public FiltroGiorno(String giorni) {
+		this.giorno = this.split(giorni);
+	}
+
+	/**
+	 * Divide i giorni passati se sono più di uno
+	 * 
+	 * @param giorni I giorni da filtrare scritte in CSV (separati da virgola)
+	 * @return Un vettore di giorni da filtrare 
+	 */
+	public String[] split(String giorni) {
+		if (giorni != null && giorni.length() != 0) {
+			return giorni.split(",");
+		}
+		return null;
+	}
+
+	/**
+	 * Filtra gli eventi con i giorni (giorni)
+	 */
+	public void filtra() {
+		if (this.giorno != null) {
+			
+			List<Evento> temp = new Vector<>();
+			for (Evento e : super.getEventiFiltrati()) {
+
+				for (int i = 0; i < giorno.length; i++) {
+
+					if (e.getData().getGiorno().equals(this.giorno[i].trim())) {
+						temp.add(e);
+					}
+
+				}
+			}
+			super.setEventiFiltrati(temp);
+		}else {
+			super.setEventiFiltrati(super.getEventiFiltrati());
+		}
+	}
+}
