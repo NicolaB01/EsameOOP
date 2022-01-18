5386d26efcca74b7b5a087a2d6b7cacdb1c9c819
BiagioliNicola <93011457+NicolaB01@users.noreply.github.com>
2022-01-18 17:59:18 +0100

    commit JUnit part 1
    Test su statistiche e classi di supporto

---
+++ progetto/src/test/java/univpm/esame/progetto/testStatistiche/TestStatistiche.java
@@ -0,0 +1,44 @@
+package univpm.esame.progetto.testStatistiche;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import univpm.esame.progetto.eccezioni.ExceptionListaVuota;
+import univpm.esame.progetto.eccezioni.ExceptionPaeseInesistente;
+import univpm.esame.progetto.eccezioni.ExceptionRegioneInesistente;
+import univpm.esame.progetto.statistiche.Statistiche;
+
+
+/**
+ * Classe implementata per effetuare test unitari su alcuni statistiche 
+ * 
+ * @author Nicola Biagioli 
+ *
+ */
+class TestStatistiche {
+
+	/**
+	 * Controllo eventi totali nelle statistiche della Polonia 
+	 * @throws ExceptionPaeseInesistente  Eccezione lanciata nel momento in cui il paese immesso non coincide con Polonia o Nuova Zelanda
+	 * @throws ExceptionListaVuota  Eccezione lanciata se vengono applicati troppi filtri
+	 */
+
+	@Test
+	@DisplayName("Controllo eventi totali nelle statistiche paese(Polonia)")
+	void testStatistichePaese() throws ExceptionListaVuota, ExceptionPaeseInesistente {
+		assertEquals(465, new Statistiche("Polonia").getStatistiche().getEventi());
+	}
+
+	/**
+	 * Controllo eventi totali nelle statistiche della regione Wellington della Nuova Zelanda 
+	 * @throws ExceptionListaVuota  Eccezione lanciata se vengono applicati troppi filtri
+	 * @throws ExceptionPaeseInesistente  Eccezione lanciata nel momento in cui il paese immesso non coincide con Polonia o Nuova Zelanda
+	 * @throws ExceptionRegioneInesistente Viene lanciata quando la regione passata come argomento non è presente fra le regioni del paese passato
+	 */
+	@Test
+	@DisplayName("Controllo eventi totali nelle statistiche paese(Polonia)")
+	void testStatisticheRegione() throws ExceptionListaVuota, ExceptionPaeseInesistente, ExceptionRegioneInesistente {
+		assertEquals(9, new Statistiche("NuovaZelanda","Wellington").getStatistiche().getEventi());
+	}
+
+}
---
+++ progetto/src/test/java/univpm/esame/progetto/testStrumenti/TestStrumenti.java
@@ -0,0 +1,60 @@
+package univpm.esame.progetto.testStrumenti;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import univpm.esame.progetto.eccezioni.ExceptionListaVuota;
+import univpm.esame.progetto.eccezioni.ExceptionPaeseInesistente;
+import univpm.esame.progetto.model.DataEvento.StrumentiMesi;
+import univpm.esame.progetto.model.LuogoEvento.StrumentiRegioni;
+
+/**
+ * Classe implementata per effetuare test unitari su alcune classi di supporto
+ * 
+ * @author Nicola Biagioli 
+ *
+ */
+class TestStrumenti {
+
+	/**
+	 * Controllo che il convertitore da int a mese funzioni
+	 * @throws ExceptionListaVuota  Eccezione lanciata se vengono applicati troppi filtri
+	 */
+	@Test
+	@DisplayName("Controllo che il convertitore da int a mese funzioni")
+	void testConvertitoreToMesi() throws ExceptionListaVuota {
+		String meseInt = "01";
+		String meseString = new StrumentiMesi().toMesi(meseInt);
+
+		assertEquals("Gennaio", meseString);
+	}
+	
+	/**
+	 * Controllo che il convertitore da postal code a nomeRegione funzioni
+	 */
+	@Test
+	@DisplayName("Controllo che il convertitore da postalcode a nomeRegione funzioni")
+	void testConvertiPostalCodeInRegione() {
+		int postalCode=9600;
+		String nomeRegione = new StrumentiRegioni().convertiPostalCodeInRegione(postalCode);
+
+		assertEquals("Southland", nomeRegione);
+	}
+	
+	/**
+	 * Controlo che il metodo controlloPaese funzioni	 
+	 * @throws ExceptionPaeseInesistente  Eccezione lanciata nel momento in cui il paese immesso non coincide con Polonia o Nuova Zelanda
+     *
+	 */
+	@Test
+	@DisplayName("Controlo che il metodo controlloPaese funzioni")
+	void testControlloPaese() throws ExceptionPaeseInesistente {
+		String paese= "polonia";
+		boolean check= new StrumentiRegioni().controlloPaese(paese);
+		
+		assertEquals(true,check);
+	}
+	
+	
+
+}
